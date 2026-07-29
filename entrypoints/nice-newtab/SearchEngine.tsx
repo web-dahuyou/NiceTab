a6e29c7e45f550c571576d103292c10b3206dce1
霁月 <1423154431@qq.com>
2026-07-29 15:15:03 +0800

    fix: import useMemo in new tab search

--- entrypoints/nice-newtab/SearchEngine.tsx
+++ entrypoints/nice-newtab/SearchEngine.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect, useMemo } from 'react';
 import { Tooltip } from 'antd';
 import { settingsUtils } from '~/entrypoints/common/storage';
 import type { SearchEngine } from '~/entrypoints/types';
